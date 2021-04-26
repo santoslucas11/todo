3d08cfec978f3914a8899be02165e900fb283757
Lucas Rocha <santoslucas.dev@gmail.com>
2021-04-26 17:18:49 -0300

    Criando ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
 
 void main() {
   runApp(App());
@@ -18,17 +19,33 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+// ignore: must_be_immutable
+class HomePage extends StatefulWidget {
+  var items = <Item>[];
+  HomePage() {
+    items = [];
+
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Teste"),
-        ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(widget.items[index].title);
+        },
       ),
     );
   }
