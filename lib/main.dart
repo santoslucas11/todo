60e7a326662e35201247fd33d14d0b8dedce55c0
Lucas Rocha <santoslucas.dev@gmail.com>
2021-04-27 15:40:40 -0300

    implementando remoção de item

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     newTaskController.clear();
@@ -73,14 +79,22 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            background: Container(
+              color: Colors.red.withOpacity(0.8),
+            ),
+            onDismissed: (direction) {
+              remove(index);
             },
           );
         },
